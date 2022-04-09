fbfd14176f93c0c4d4c61a71f3b27d8cefaabf7a
lhh1171 <2807652103@qq.com>
2022-04-09 15:09:10 +0800

    cpp

---
+++ cpplang/cmake-build-debug/CMakeFiles/cpplang.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpplang.dir/main.cpp.o"
+  "cpplang"
+  "cpplang.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpplang.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cpplang/cmake-build-debug/CMakeFiles/cpplang.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cpplang.
+# This may be replaced when dependencies are built.
