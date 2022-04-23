0679f549e599b713940b98d952e1ba12cb2cf031
lhh1171 <2807652103@qq.com>
2022-04-23 21:47:15 +0800

    rr

---
+++ syscall/cmake-build-debug/CMakeFiles/syscall.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/syscall.dir/main.cpp.o"
+  "syscall"
+  "syscall.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/syscall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
