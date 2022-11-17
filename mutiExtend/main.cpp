2573ee6e5407dd98d7e10eb75c5a01767f3d34fa
lhh1171 <2807652103@qq.com>
2022-11-17 20:10:27 +0800

    mutiExtend

---
+++ mutiExtend/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+class A{
+    int id;
+public:
+    A(int id):id(id){
+        cout<<"A"<<endl;
+    }
+    ~A(){
+        cout<<"~A"<<endl;
+    }
+};
+class B:virtual public A{
+    int xxx;
+public:
+    B(int id,int xxx):A(id),xxx(xxx){
+        cout<<"B"<<endl;
+    }
+};
+class C:virtual public A{
+    int xxx;
+public:
+    C(int id,int xxx):A(id),xxx(xxx){
+        cout<<"C"<<endl;
+    }
+};
+//class D: public B, public C{
+//    int yyy;
+//public:
+//    D(int id,int xxx,int yyy):B(id,xxx),C(id,xxx),yyy(yyy){
+//        cout<<"D"<<endl;
+//    }
+//};
+class D: public B, public C{
+    int yyy;
+public:
+    D(int id,int xxx,int yyy):A(id),B(id,xxx),C(id,xxx),yyy(yyy){
+        cout<<"D"<<endl;
+    }
+};
+int main() {
+    D d(1,2,3);
+    //虚拟继承,解决菱形继承问题
+    return 0;
+}
